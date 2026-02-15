51d17ef124d4d546d5e09b06708ae8bbb17b19dd
Avenger <nitroplays1234@gmail.com>
2026-02-15 18:40:24 +0545

    Refactor render-build.sh to simplify build process
    Removed fake tput creation and adjusted build messages.

--- render-build.sh
+++ render-build.sh
@@ -2,22 +2,16 @@
 # exit on error
 set -o errexit
 
-# Create a fake tput so Render's internal script stops crashing
-mkdir -p internal_bin
-echo -e '#!/bin/sh\nexit 0' > internal_bin/tput
-chmod +x internal_bin/tput
+# Disable terminal colors to stop the tput error
+export TERM=dumb
 
-# Temporarily add our fake tput to the path for the build
-export PATH=$(pwd)/internal_bin:$PATH
-
-echo "--- Starting Real Build ---"
-
+echo "Building dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# Download FFmpeg
+echo "Installing FFmpeg..."
 mkdir -p bin
 curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz | tar -xJ --strip-components=1 -C bin
 chmod +x bin/ffmpeg
 
-echo "--- Build Finished Successfully ---"
+echo "Build complete!"
